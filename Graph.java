bda10154afc072a7d27a9d14043bfefbdece0db5
ekambains <bainseka@sheridancollege.ca>
2025-03-21 13:36:48 -0400

    Added Graph Data Structure

---
+++ Graph.java
@@ -0,0 +1,26 @@
+//Graph Using Adjacency Matrix
+
+public class Graph {
+    int[][] matrix;
+
+    Graph(int size) {
+        matrix = new int[size][size];
+    }
+
+    public void addEdge(int src, int dst) {
+        matrix[src][dst] = 1;
+    }
+
+    public boolean checkEdge(int src, int dst) {
+        return (matrix[src][dst] == 1) ? true : false;
+    }
+
+    public void print() {
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j < matrix.length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
